7f5eddd6b3c62fe0ab129337e8408c035515da79
Dragonaere <andrew@dragonaere.tech>
2022-01-21 15:47:08 -0500

    added some things that dont work...

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,12 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Client, Collection, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('ping')
 		.setDescription('Gets the Bots ping!'),
 	async execute(interaction) {
-		await interaction.reply(`🏓Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+		await interaction.reply(`Latency is ${Interaction.ping}ms. API Latency is ${Math.round(client.ws)}ms`);
 	},
 };
