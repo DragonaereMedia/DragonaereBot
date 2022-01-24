aee5e7eac2c95467be949d8384756e25797b44fb
Andrew Still <ats.gcs.2016@gmail.com>
2022-01-24 11:01:59 -0500

    fixed user.js being stoopid

--- commands/user.js
+++ commands/user.js
@@ -5,21 +5,21 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
 		.setDescription('Replies with user info!'),
-	async execute(interaction) {
-        interaction.user.fetchFlags(true)
-        interaction.user.fetch(true)
-        userTag=interaction.user.tag
-        userAvatar=interaction.user.displayAvatarURL('webp',128,true)
-        //userBanner=interaction.user.bannerURL('webp',512,true)
-        const userEmbed = new MessageEmbed()
-		.setColor('#0099ff')
-		.setTitle(userTag)
-		.setThumbnail(userAvatar)
-		//.addField(userBanner)
-        .addField(`Account Created ${interaction.user.createdAt}`)
-        .addField(`Joined Server ${interaction.guild.joinedAt`)
-		.setTimestamp()
-		.setFooter({text:'/user'});
-		await interaction.reply({embeds: [userEmbed]});
-	},
+		async execute(interaction) {
+        		interaction.user.fetchFlags(true)
+        		interaction.user.fetch(true)
+        		userTag=interaction.user.tag
+	        	userAvatar=interaction.user.displayAvatarURL('webp',128,true)
+        		//userBanner=interaction.user.bannerURL('webp',512,true)
+        		const userEmbed = new MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle(userTag)
+			.setThumbnail(userAvatar)
+			//.addField(userBanner)
+        		.addField(`Account Created ${interaction.user.createdAt}`)
+        		.addField(`Joined Server ${interaction.guild.joinedAt`)
+			.setTimestamp()
+			.setFooter({text:'/user'});
+			await interaction.reply({embeds: [userEmbed]});
+		},
 };
