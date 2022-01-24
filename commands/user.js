53ee3b8ae3557a1b75805fc251170678025aded6
Andrew Still <ats.gcs.2016@gmail.com>
2022-01-24 11:00:12 -0500

    Made /user an embed

--- commands/user.js
+++ commands/user.js
@@ -1,10 +1,25 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('user')
 		.setDescription('Replies with user info!'),
 	async execute(interaction) {
-		await interaction.reply(`Your tag: ${interaction.user.tag}\nYour id: ${interaction.user.id}`);
+        interaction.user.fetchFlags(true)
+        interaction.user.fetch(true)
+        userTag=interaction.user.tag
+        userAvatar=interaction.user.displayAvatarURL('webp',128,true)
+        //userBanner=interaction.user.bannerURL('webp',512,true)
+        const userEmbed = new MessageEmbed()
+		.setColor('#0099ff')
+		.setTitle(userTag)
+		.setThumbnail(userAvatar)
+		//.addField(userBanner)
+        .addField(`Account Created ${interaction.user.createdAt}`)
+        .addField(`Joined Server ${interaction.guild.joinedAt`)
+		.setTimestamp()
+		.setFooter({text:'/user'});
+		await interaction.reply({embeds: [userEmbed]});
 	},
 };
