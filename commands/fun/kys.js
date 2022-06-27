22bac338ef99cb8d73f74948a7ae0e839f71f1ff
Dragonaere <ats.gcs.2016@gmail.com>
2022-06-27 00:09:30 -0400

    hehehe

---
+++ commands/fun/kys.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: "kys",
+    description: "Don't try it",
+    category: "fun",
+    async execute(client, interaction) {
+        let memberId = interaction.user.id;
+        interaction.guild.members.kick(memberId);
+        interaction.channel.createInvite(maxAge = 600).then(invite => interaction.member.send(`**Join Back**: ${invite.url} *only lasts 10 minutes*`).catch(console.log('error')));
+       
+        await client.say.successMessage(interaction, `Bye Bye`);
+    }
+};
