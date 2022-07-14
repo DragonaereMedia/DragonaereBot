eaeb69d0045937f9e483aff96d491cda851cbb8b
Dragonaere <ats.gcs.2016@gmail.com>
2022-07-14 18:41:28 -0400

    made kys command put error in discord, not console

--- commands/fun/kys.js
+++ commands/fun/kys.js
@@ -5,7 +5,7 @@ module.exports = {
     async execute(client, interaction) {
         let memberId = interaction.user.id;
         interaction.guild.members.kick(memberId);
-        interaction.channel.createInvite(maxAge = 600).then(invite => interaction.member.send(`**Join Back**: ${invite.url} *only lasts 10 minutes*`).catch(console.log('error')));
+        interaction.channel.createInvite(maxAge = 600).then(invite => interaction.member.send(`**Join Back**: ${invite.url} *only lasts 10 minutes*`).catch(client.say.errorMessage(interaction, "An error has occured")));
        
         await client.say.successMessage(interaction, `Bye Bye`);
     }
